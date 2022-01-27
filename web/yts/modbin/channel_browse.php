d841b26d67fa3ace333dcd422a4eb427e0f0137b
Hwilliams8548 <68089490+Hwilliams8548@users.noreply.github.com>
2022-01-27 18:22:30 -0500

    Update channel_browse.php

--- web/yts/modbin/channel_browse.php
+++ web/yts/modbin/channel_browse.php
@@ -5,66 +5,24 @@
 
                     <div class="compact-shelf shelf-item yt-uix-flex-width-slider yt-uix-flex-width-slider-at-head yt-uix-flex-width-slider-default-style c4-box">
         <h2 class="branded-page-module-title">
-      <a class="spf-link" href="/web/20121226175335/http://www.youtube.com/channel/HC7Dr1BKwqctY/videos?query=dMIND5LSebE&amp;view=22&amp;feature=g-logo" title="Fitness" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D&amp;ved=CBQQzh4%3D">
+      <a class="spf-link" href="/user/<?php echo $user[0]; ?>/videos?query=dMIND5LSebE&amp;view=22&amp;feature=g-logo" title="Fitness">
         Fitness
       </a>
     </h2>
 
 
-    <button type="button" class="yt-uix-flex-width-slider-prev yt-uix-button yt-uix-button-shelf-slider-pager" onclick=";return false;" role="button"><span class="yt-uix-button-content">  <img class="yt-uix-slider-prev-arrow" src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="Previous">
- </span></button>
-    <button type="button" class="yt-uix-flex-width-slider-next yt-uix-button yt-uix-button-shelf-slider-pager" onclick=";return false;" role="button"><span class="yt-uix-button-content">  <img class="yt-uix-slider-next-arrow" src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="Next">
- </span></button>
-
-      <div class="yt-uix-flex-width-slider-body context-data-container">
-    <ul class="yt-uix-flex-width-slider-list">
-        <li class="channels-content-item yt-uix-flex-width-slider-item">
-                <span class="context-data-item" data-context-item-views="7,910 views" data-context-item-user="flexforall2" data-context-item-title="Matt Versus 2.6" data-context-item-type="video" data-context-item-id="v_aHZ_pc8k0" data-context-item-time="8:35">
-    <a href="/web/20121226175335/http://www.youtube.com/watch?v=v_aHZ_pc8k0" class="ux-thumb-wrap yt-uix-sessionlink yt-uix-contextlink contains-addto spf-link" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D"><span class="video-thumb ux-thumb yt-thumb-default-167 "><span class="yt-thumb-clip"><span class="yt-thumb-clip-inner"><img src="//web.archive.org/web/20121226175335im_/http://i3.ytimg.com/vi/v_aHZ_pc8k0/mqdefault.jpg" alt="Thumbnail" width="167"><span class="vertical-align"></span></span></span></span><span class="video-time">8:35</span>
-
-
-  <button onclick=";return false;" title="Watch Later" type="button" class="addto-button video-actions spf-nolink addto-watch-later-button-sign-in yt-uix-button yt-uix-button-hh-default yt-uix-button-short yt-uix-tooltip" data-video-ids="v_aHZ_pc8k0" data-button-menu-id="shared-addto-watch-later-login" role="button"><span class="yt-uix-button-content">  <img src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="Watch Later">
- </span><img class="yt-uix-button-arrow" src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt=""></button>
-</a>
     <span class="content-item-detail">
-      <a href="/web/20121226175335/http://www.youtube.com/watch?v=v_aHZ_pc8k0" title="Matt Versus 2.6" class="content-item-title spf-link yt-uix-sessionlink yt-uix-contextlink" dir="ltr" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D">
-        Matt Versus 2.6
+      <a href="/soon.php?context=video" title="video" class="content-item-title spf-link yt-uix-sessionlink yt-uix-contextlink" dir="ltr">
+        video
       </a>
       <span class="content-item-metadata">
         
         
           <span class="content-item-view-count">
-            7,910 views
+            69 views
           </span>
           <span class="metadata-separator">|</span>
-          <span class="username-prepend">by</span> <a href="/web/20121226175335/http://www.youtube.com/user/flexforall2" class="yt-uix-sessionlink yt-user-name spf-link" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D" dir="ltr">flexforall2</a>
-
-      </span>
-    </span>
-  </span>
-
-
-        </li>
-        <li class="channels-content-item yt-uix-flex-width-slider-item last">
-                <span class="context-data-item" data-context-item-views="6,006 views" data-context-item-user="strengthcamp" data-context-item-title="Get Bigger with Only Calisthenics" data-context-item-type="video" data-context-item-id="U34_O2GQRes" data-context-item-time="4:32">
-    <a href="/web/20121226175335/http://www.youtube.com/watch?v=U34_O2GQRes" class="ux-thumb-wrap yt-uix-sessionlink yt-uix-contextlink contains-addto spf-link" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D"><span class="video-thumb ux-thumb yt-thumb-default-167 "><span class="yt-thumb-clip"><span class="yt-thumb-clip-inner"><img src="//web.archive.org/web/20121226175335im_/http://i2.ytimg.com/vi/U34_O2GQRes/mqdefault.jpg" alt="Thumbnail" width="167"><span class="vertical-align"></span></span></span></span><span class="video-time">4:32</span>
-
-
-  <button onclick=";return false;" title="Watch Later" type="button" class="addto-button video-actions spf-nolink addto-watch-later-button-sign-in yt-uix-button yt-uix-button-hh-default yt-uix-button-short yt-uix-tooltip" data-video-ids="U34_O2GQRes" data-button-menu-id="shared-addto-watch-later-login" role="button"><span class="yt-uix-button-content">  <img src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="Watch Later">
- </span><img class="yt-uix-button-arrow" src="//web.archive.org/web/20121226175335im_/http://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt=""></button>
-</a>
-    <span class="content-item-detail">
-      <a href="/web/20121226175335/http://www.youtube.com/watch?v=U34_O2GQRes" title="Get Bigger with Only Calisthenics" class="content-item-title spf-link yt-uix-sessionlink yt-uix-contextlink" dir="ltr" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D">
-        Get Bigger with Only Calisthenics
-      </a>
-      <span class="content-item-metadata">
-        
-        
-          <span class="content-item-view-count">
-            6,006 views
-          </span>
-          <span class="metadata-separator">|</span>
-          <span class="username-prepend">by</span> <a href="/web/20121226175335/http://www.youtube.com/user/strengthcamp" class="yt-uix-sessionlink yt-user-name spf-link" data-sessionlink="ei=CPqTquDRuLQCFVaMIQod_DS2GA%3D%3D" dir="ltr">strengthcamp</a>
+          <span class="username-prepend">by</span> <a href="/user/falcontube" class="yt-uix-sessionlink yt-user-name spf-link" dir="ltr">FalconTube</a>
 
       </span>
     </span>
