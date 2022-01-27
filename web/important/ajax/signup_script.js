6a244ff18cf0ba15e7bc21733708e005b75121ff
SucculentDevKit <59074434+Nintendo-png@users.noreply.github.com>
2022-01-26 20:40:11 -0700

    Update signup_script.js

--- web/important/ajax/signup_script.js
+++ web/important/ajax/signup_script.js
@@ -1,24 +1,64 @@
-function signup_Validator() {
-	var e = document.getElementById("email"),
-		a = document.getElementById("username"),
-		t = document.getElementById("gaia_loginform"),
-		n = document.getElementById("pass"),
-		s = document.getElementById("Passwd-2"),
-		l = document.getElementById("signup"),
-		i = document.getElementById("password-meter-bar"),
-		r = i.className;
-	if(l.disabled = "true", l.value = "Please wait...", "" == n.value || "" == s.value || "" == e.value || "" == a.value) return alert("Please make sure all fields are filled."), l.value = "Sign Up", void l.removeAttribute("disabled");
-	if(-1 !== r.indexOf("level0")) return alert("Please use a stronger password."), l.value = "Sign Up", void l.removeAttribute("disabled");
-	if(n.value != s.value) return alert("Please make sure both passwords match."), l.value = "Sign Up", void l.removeAttribute("disabled");
-	const d = new XMLHttpRequest;
-	d.onload = function() {
-		if(1 == this.responseText) alert("This username has been used."), l.value = "Sign Up", l.removeAttribute("disabled");
-		else if(3 == this.responseText) alert("Username is not allowed. Please type in a different username."), l.value = "Sign Up", l.removeAttribute("disabled");
-		else {
-			var e = new FormData(t);
-			d.onload = function() {
-				1 == this.responseText && (window.location.href = "/")
-			}, d.open("POST", "important/ajax/signup_ajax.php", !0), d.send(e)
-		}
-	}, d.open("GET", "important/ajax/signup_ajax.php?uname=" + a.value, !0), d.send()
+
+function signup_Validator()
+{
+	//elements
+	var signup_email = document.getElementById('email');
+	var signup_username = document.getElementById('username');
+	var signup_form = document.getElementById('gaia_loginform');
+	var password_1 = document.getElementById('pass');
+	var password_2 = document.getElementById('Passwd-2');
+	var signup_button = document.getElementById('signup');
+	var pass_meter = document.getElementById('password-meter-bar');
+	var pass_check2 = pass_meter.className;
+//starting point
+	signup_button.disabled='true';
+	signup_button.value='Please wait...';
+	if((password_1.value == "") || (password_2.value == "") || (signup_email.value == "") || (signup_username.value == "")){
+					alert("Please make sure all fields are filled.");
+					signup_button.value='Sign Up';
+	                signup_button.removeAttribute('disabled');
+					return;
+	} else if(pass_check2.indexOf("level0") !== -1) {
+					alert("Please use a stronger password.");
+					signup_button.value='Sign Up';
+	                signup_button.removeAttribute('disabled');
+					return;
+	} else {
+	if(password_1.value == password_2.value) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    if(this.responseText == 1) {
+			alert("This username has been used.");
+		signup_button.value='Sign Up';
+	signup_button.removeAttribute('disabled');
+	} else if(this.responseText == 3) {
+			alert("Username is not allowed. Please type in a different username.");
+		signup_button.value='Sign Up';
+	signup_button.removeAttribute('disabled');
+	} else {
+		//signup system
+		//really shitty, the point of this revival
+		var formData = new FormData(signup_form);
+		  xhttp.onload = function() {
+    if(this.responseText == 1) {
+		window.location.href = "/";
+	}
+		  }
+xhttp.open("POST", "important/ajax/signup_ajax.php", true);
+  xhttp.send(formData);
+  
+	}
+	
+    }
+  xhttp.open("GET", "important/ajax/signup_ajax.php?uname=" + signup_username.value, true);
+  xhttp.send();
+
+	} else {
+					alert("Please make sure both passwords match.");
+					signup_button.value='Sign Up';
+	                signup_button.removeAttribute('disabled');
+	return;
+	}
+	}
 }
+   
